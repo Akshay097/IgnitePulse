7b4276e63d3baaae7203cb86593209563125c456
Akshay Akshay <akshay.akshay@ibm.com>
2025-04-21 17:12:31 -0300

    Reduce geofence radius to 10 meters

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ with open("whitelist.json", "r") as f:
 # Set your office geolocation (example: Halifax, NS)
 OFFICE_LAT = 44.72338559753693
 OFFICE_LON = -63.6954247294425
-GEOFENCE_RADIUS_KM = 0.15  # 150 meters
+GEOFENCE_RADIUS_KM = 0.01  # 150 meters
 
 def haversine(lat1, lon1, lat2, lon2):
     R = 6371  # Earth radius in km
